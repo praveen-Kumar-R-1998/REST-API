62633bbd46028072d18015f5ffe3df5a26e512d5
praveen <praveenravichandran2051@gmail.com>
2024-09-22 00:21:34 +0530

    Dynamic filtering implementation.

--- src/main/java/com/in28minuter/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/in28minuter/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -3,21 +3,62 @@ package com.in28minuter.rest.webservices.restfulwebservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 
+	/**
+	 * Static filtering
+	 */
 	@GetMapping("/filtering")
 	public SomeBean filtering() {
 		return new SomeBean("value 1", "value 2", "value 3");
 	}
-	
+
 	@GetMapping("/filtering-list")
 	public List<SomeBean> filteringList() {
 		return Arrays.asList(new SomeBean("value 1", "value 2", "value 3"),
-				new SomeBean("value 1", "value 2", "value 3"),
-				new SomeBean("value 1", "value 2", "value 3"));
+				new SomeBean("value 1", "value 2", "value 3"), new SomeBean("value 1", "value 2", "value 3"));
+	}
+
+	/**
+	 * Dynamic Filtering
+	 */
+
+	@GetMapping("/filtering-dynamic")
+	public MappingJacksonValue filteringDynamic() {
+
+		SomeBean someBean = new SomeBean("value 1", "value 2", "value 3", "value 4");
+
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
+
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+		mappingJacksonValue.setFilters(filters);
+
+		return mappingJacksonValue;
+	}
+
+	@GetMapping("/filtering-dynamic-list")
+	public MappingJacksonValue filteringDynamiclist() {
+
+		List<SomeBean> list = Arrays.asList(new SomeBean("value 1", "value 2", "value 3", "value 4"),
+				new SomeBean("value 1", "value 2", "value 3","value 4"),
+				new SomeBean("value 1", "value 2", "value 3", "value 4"));
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(list);
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field4");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mappingJacksonValue.setFilters(filters);
+		return mappingJacksonValue;
+
 	}
 }
--- src/main/java/com/in28minuter/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/in28minuter/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,17 +1,21 @@
 package com.in28minuter.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties("field1")
+//@JsonIgnoreProperties("field1")
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 	
 	public String field1;
 	
-	@JsonIgnore
+//	@JsonIgnore
 	public String field2;
 	
 	public String field3;
+	
+	public String field4;
 
 	public SomeBean(String value1, String value2, String value3) {
 		super();
@@ -19,6 +23,18 @@ public class SomeBean {
 		this.field2 = value2;
 		this.field3 = value3;
 	}
+	
+	
+
+	public SomeBean(String field1, String field2, String field3, String field4) {
+		super();
+		this.field1 = field1;
+		this.field2 = field2;
+		this.field3 = field3;
+		this.field4 = field4;
+	}
+
+
 
 	public String getField1() {
 		return field1;
@@ -43,10 +59,25 @@ public class SomeBean {
 	public void setField3(String field3) {
 		this.field3 = field3;
 	}
+	
+	
+	public String getField4() {
+		return field4;
+	}
+
+
+
+	public void setField4(String field4) {
+		this.field4 = field4;
+	}
+
+
 
 	@Override
 	public String toString() {
-		return "SomeBean [field1=" + field1 + ", field2=" + field2 + ", field3=" + field3 + "]";
+		return "SomeBean [field1=" + field1 + ", field2=" + field2 + ", field3=" + field3 + ", field4=" + field4 + "]";
 	}
+	
+	
 
 }
